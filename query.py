da41d187f47a01427d832b4b98ee85da48ed06df
Ellen <124838733+eyu776@users.noreply.github.com>
2023-10-27 02:41:10 +0000

    feat: Add menu functionality to query.py
    Implemented menu functionality to query.py by adding code that
    continuously prompts the user until they choose the option to quit. The
    code only executes options that are spelled correctly by the user and is
    not case-sensitive. When the user enters a correct option, the code will
    execute the respective desired function and then prompt the user again
    with the menu.
    
    issue #1

--- query.py
+++ query.py
@@ -1,3 +1,19 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+def menu() -> None:
+  print("q: quit\nsn: search movie names\nsc: search casts\nlist: print all movie names")
+  x = input("").strip().lower()
+  if x == "q": return None
+  else:
+    if x == "sn":
+      print("call search names function")
+    elif x == "sc":
+      print("call search casts function")
+    elif x == "list":
+      print("call list function")
+    print()
+    menu()
+
+menu()
